508784eea9e2f1e50f92334e42e4a953a413c3d8
Jeff Hanson <jeffrey.hanson@uqconnect.edu.au>
2016-08-02 14:03:07 +1000

    + fix bug in point buffering

--- code/python/buffer.py
+++ code/python/buffer.py
@@ -16,7 +16,7 @@ arcpy.env.overwriteOutput = True
 ### Main processing
 # calculate radius
 arcpy.AddField_management(sys.argv[1], 'REP_RADIUS', 'DOUBLE')
-arcpy.CalculateField_management(sys.argv[1], 'REP_RADIUS', 'math.sqrt(!REP_AREA!/math.pi())')
+arcpy.CalculateField_management(sys.argv[1], 'REP_RADIUS', 'math.sqrt(!REP_AREA!/math.pi)', 'PYTHON_9.3')
 
 # buffer points to polygon
 arcpy.Buffer_analysis(sys.argv[1], sys.argv[2], 'REP_RADIUS')
